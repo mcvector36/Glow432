434be2d5367e14fa2f066ae36904d80547b18f3e
mcvector36 <constantinmihai804@gmail.com>
2025-04-05 17:08:16 +0300

    standalone

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
-#include "floatingbutton.h"
 #include "sound432.h"
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Setează icoana aplicației
+    a.setWindowIcon(QIcon(":/icon.ico"));  // Calea corectă către icoana din resurse
+
+
     Sound432 w;
-
-    // Ascunde fereastra principală
-    w.hide();
-
+    w.show();
     return a.exec();
 }
--- sound432.cpp
+++ sound432.cpp
@@ -1,12 +1,17 @@
 #include "sound432.h"
 #include "ui_sound432.h"
 #include "floatingbutton.h"
+#include <QIcon>  // Asigură-te că ai importat QIcon
 
 Sound432::Sound432(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Sound432)
 {
     ui->setupUi(this);
+
+    // Setează icoana aplicației
+    QIcon appIcon(":/icon.ico");  // Calea către icoana din fișierul .qrc
+    setWindowIcon(appIcon);  // Setează icoana ferestrei aplicației
 
     // Ascunde fereastra principală
     this->hide();
